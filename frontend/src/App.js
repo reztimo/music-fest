d4149a1440c2bd1ac919ef2805724b0fd47bf2d8
reztimo <maulanamuhammad182000@gmail.com>
2024-01-08 16:22:56 +0700

    add login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Login from './userAction/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Register from './userAction/Register';
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/userAction/Login.js
+++ frontend/src/userAction/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import bg from "../assets/bg.jpg";
-import { Link } from 'react-router'
 
 const Login = () => {
     return (
@@ -11,6 +10,9 @@ const Login = () => {
                     <div className='md:mx-auto md:w-full md:max-w-full py-5'>
                         <form className='space-y-6' action='$' method='POST'>
                             <div>
+                                <label htmlFor='username' className="block text-sm font-medium leading-6 text-white">
+                                    Your Username
+                                </label>
                                 <div className='mt-2'>
                                     <input
                                         id=''
@@ -19,11 +21,11 @@ const Login = () => {
                                         className='block w-full px-2 border-0 border-b-2 bg-transparent py-1.5 text-white border-gray-300 appearance-none dark:focus:border-white-800 focus:outline-none focus:ring-0 focus-text-white focus-border-blue-600 peer sm:text-sm sm:leading-6'
                                     />
                                 </div>
-                                <label htmlFor='username' className="block text-sm font-medium leading-6 text-white">
-                                    Your Username
-                                </label>
                             </div>
                             <div>
+                                <label htmlFor='password' className="block text-sm font-medium leading-6 text-white">
+                                    Your Password
+                                </label>
                                 <div className='mt-2'>
                                     <input
                                         id=''
@@ -32,18 +34,18 @@ const Login = () => {
                                         className='block w-full px-2 border-0 border-b-2 bg-transparent py-1.5 text-white border-gray-300 appearance-none dark:focus:border-white-800 focus:outline-none focus:ring-0 focus-text-white focus-border-blue-600 peer sm:text-sm sm:leading-6'
                                     />
                                 </div>
-                                <label htmlFor='password' className="block text-sm font-medium leading-6 text-white">
-                                    Your Password
-                                </label>
                             </div>
                             <div>
                                 <button type="submit" className='flex w-full justify-center rounded-md text-gray-800 bg-white px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Login</button>
                             </div>
                             <div>
+                                <a href="&" className='block text-sm font-medium leading-6 text-white'>Forget Password?</a>
                             </div>
                             <div>
-                                <input type="checkbox" name="" id="" />
-                                <label htmlFor='Remember Me' className='text-white px-6'>Remember Me</label>
+                                <div className='flex items-center'>
+                                    <input className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" type="checkbox" name="" id="" />
+                                    <label htmlFor='Remember Me' className='text-sm font-medium text-white px-3 content-start'>Remember Me</label>
+                                </div>
                             </div>
                             <div>
                                 <button type="submit" className='flex w-full justify-center rounded-md text-gray-800 bg-white px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>Register</button>
